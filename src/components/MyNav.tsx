955a009d12b73f4c3f3986caca5460b9c4c3ba58
Steven M. Drucker <sdrucker@microsoft.com>
2018-12-21 17:22:24 -0800

    adding routing

---
+++ src/components/MyNav.tsx
@@ -0,0 +1,51 @@
+import '../../styles/main.scss';
+import '../../styles/bootstrap.min.css';
+import '../../styles/bootstrap-theme.css';
+import '../../styles/slick.scss'
+
+import { Link, LinkContainer } from 'react-router'
+import {Navbar, Nav, NavItem, Row, Col} from 'react-bootstrap'
+import * as React from 'react';
+
+export class MyNav extends React.Component<any, any> { 
+  render() {
+    return (
+      <div>
+          <Row>
+          <Col lg={1} md={1} sm={1}>
+          </Col>
+          <Col lg={8} md={8} sm={8}>
+           <Navbar>        
+            <Navbar.Header>
+              <Navbar.Toggle />
+            </Navbar.Header>
+            <Navbar.Collapse>
+              <Nav bsStyle="pills">  
+                <LinkContainer to="/about">
+                    <NavItem eventKey={1}> Bio </NavItem>
+                </LinkContainer>
+                <LinkContainer to="/one">
+                    <NavItem eventKey={2}> Featured </NavItem>
+                </LinkContainer>
+                <LinkContainer to="/two">
+                    <NavItem eventKey={3}> Research </NavItem>
+                </LinkContainer>
+                <LinkContainer to="/one">
+                    <NavItem eventKey={4}> CV </NavItem>
+                </LinkContainer>         
+         
+              </Nav>
+            </Navbar.Collapse>
+            </Navbar>
+          </Col>          
+          <Col lg={1} md={1} sm={1}>
+          </Col>        
+        </Row>
+        <div className="spacer"  />
+        <Row>
+          {this.props.children}
+        </Row>
+      </div>
+    )
+  } 
+}
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,55 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import JSON from "../data/researchData.json"
+import { BrowserRouter as Router, Route, Link, hashHistory} from 'react-router-dom'
+import {About} from "./components/About"
 
-const app = (         
-        <section> Hello Steven! {JSON[0].booktitle}</section>     
+  
+class One extends React.Component<any, any> {
+    render() {
+      return (
+        <div>
+          <div>ONE</div>
+        </div>
+      );
+    }
+}
+  
+class Two extends React.Component<any, any> {
+    render() {
+      return (
+        <div>
+          <div>TWO</div>          
+        </div>
+      );
+    }
+}
 
-);
+class Root extends React.Component<any, any> {
+    render() {
+        return(            
+            <Router history={hashHistory}>                
+            <div>         
+            <ul>
+                    <li> 
+                        <Link to="/about">About</Link>
+                    </li>
+                    <li>
+                        <Link to="/one">One</Link>
+                    </li>
+                    <li>
+                        <Link to="/two">Two</Link>
+                    </li>
+                    </ul>
+                    <Route path="/" component={About}/>
+                    <Route path="/about" component={About}/>
+                    <Route path="/one" component={One}/>
+                    <Route path="/two" component={Two}/>                                     
+            </div>
+        </Router>);
+    }
+}
+ReactDOM.render( 
+    <Root />
 
-ReactDOM.render(app, document.getElementById('app'));
+, document.getElementById('app')); 
