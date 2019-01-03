3052273df8cab3fe290f470dd46f84bb394a1fe9
Steven M. Drucker <sdrucker@microsoft.com>
2019-01-03 15:37:03 -0800

    updated CV and data

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,5 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import JSON from "../data/researchData.json"
 import { BrowserRouter as Router, Route, Link, hashHistory} from 'react-router-dom'
 import {About} from "./components/About"
 import { MyNav } from './components/MyNav';
